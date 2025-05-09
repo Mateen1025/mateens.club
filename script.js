f4f0d2f6156a625c7cfa8a3f0431c074b3572c70
Mateen Ahsan <kmateen70791@gmail.com>
2025-05-08 18:04:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//function checkPassword() {
+  //  var password = prompt("Please enter the password:");
+    
+    //if (password === "frankbutt") { 
+      //  window.location.href = "Transcript.html"; 
+    //} else {
+      //  alert("Incorrect password. Please try again.");
+   // }
+//}
 function checkPassword() {
     var password = prompt("Please enter the password:");
     
